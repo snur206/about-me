3349becd999adc4768f80c054d0970620603bd75
SHARMARKE NUR <Snur206@yahoo.com>
2022-10-27 10:15:24 -0700

    about-me

--- js/app.js
+++ js/app.js
@@ -11,10 +11,10 @@ alert(`Hello and welcome to my page ${visitorName}! I Would like for you to answ
 let questionOneGuess = prompt('Do I drink or enjoy coffee?').toUpperCase();
 
 if(questionOneGuess === 'YES' || questionOneGuess === 'Y') {
-  // console.log('Incorrect. Even though I am from the city known about our coffee.');
+  console.log('Incorrect. Even though I am from the city known about our coffee.');
     alert('Incorrect. Even though I am from the city known about our coffee.');
 } else if (questionOneGuess === 'NO' || questionOneGuess === 'N') {
-  // console.log('Correct.');
+  console.log('Correct.');
     alert('Correct.');
     answer = answer + 1;
 }
@@ -24,9 +24,9 @@ let questionTwoGuess = prompt('Do you play sports?').toUpperCase();
 
 if(questionTwoGuess === 'YES' || questionTwoGuess === 'Y') {
   alert('That is great! I do also.');
-  // console.log('That is great! I do also.');
+  console.log('That is great! I do also.');
 } else if (questionTwoGuess === 'NO' || questionTwoGuess === 'N') {
-    // console.log('That if fine. Everyone has their own interest.');
+  console.log('That if fine. Everyone has their own interest.');
   alert('That if fine. Everyone has their own interest.');
   answer = answer + 2;
 }
@@ -35,10 +35,10 @@ if(questionTwoGuess === 'YES' || questionTwoGuess === 'Y') {
 let questionThreeGuess = prompt('Can I speak more than one language?').toUpperCase();
 
 if(questionThreeGuess === 'YES' || questionThreeGuess === 'Y') {
-    // console.log('Correct!');
+  console.log('Correct!');
   alert('Correct! I can speak English and Somali.');
 } else if (questionThreeGuess === 'NO' || questionThreeGuess === 'N') {
-    // console.log('Incorrect, I can speak English and Somali.');
+  console.log('Incorrect, I can speak English and Somali.');
   alert('Incorrect, I can speak English and Somali.');
   answer = answer + 3;
 }
@@ -46,10 +46,10 @@ if(questionThreeGuess === 'YES' || questionThreeGuess === 'Y') {
 let questionFourGuess = prompt('Have I had to get any surgeries??').toUpperCase();
 
 if(questionFourGuess === 'YES' || questionFourGuess === 'Y') {
-    // console.log('Nope. Thankfully the only injury I sustained was a knee sprain.');
+  console.log('Nope. Thankfully the only injury I sustained was a knee sprain.');
   alert('Nope. Thankfully the only injury I sustained was a knee sprain.');
 } else if (questionFourGuess === 'NO' || questionFourGuess === 'N') {
-    // console.log('Yes. Lucky enough to not need any type of surgery.');
+  console.log('Yes. Lucky enough to not need any type of surgery.');
   alert('Yes. Lucky enough to not need any type of surgery.');
   answer = answer + 4
 }
@@ -58,10 +58,10 @@ if(questionFourGuess === 'YES' || questionFourGuess === 'Y') {
 let questionFiveGuess = prompt('Do I have any tattoos?').toUpperCase();
 
 if(questionFiveGuess === 'YES' || questionFiveGuess === 'Y') {
-    // console.log('Wrong. It is against my religion.');
+  console.log('Wrong. It is against my religion.');
   alert('Wrong. It is against my religion.');
 } else if (questionFiveGuess === 'NO' || questionFiveGuess === 'N') {
-    // console.log('Correct!');
+  console.log('Correct!');
   alert('Correct!');
   answer = answer + 5;
 }
@@ -114,6 +114,8 @@ while (guesses > 0) {
   }
   guesses --;
 }
+console.log('Correct answers were ' + seattle);
 alert('Correct answers were ' + seattle);
 
+console.log("You got " + answer + " out of 7 questions right!");
 alert("You got " + answer + " out of 7 questions right!");
